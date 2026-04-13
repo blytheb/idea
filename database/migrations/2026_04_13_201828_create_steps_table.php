a145c4083c5b764de5d327ddc6d30ba1a5a7f839
blytheb <blytheballesteros@gmail.com>
2026-04-13 12:50:34 -1000

    building factories and tests

--- database/migrations/2026_04_13_201828_create_steps_table.php
+++ database/migrations/2026_04_13_201828_create_steps_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Idea;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +14,7 @@ return new class extends Migration
     {
         Schema::create('steps', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(Idea::class);
             $table->string('description');
             $table->boolean('completed');
             $table->timestamps();
