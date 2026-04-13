def12fbe7e8173058f20f765d4a9f38d6bdeaf10
blytheb <blytheballesteros@gmail.com>
2026-04-13 13:46:59 -1000

    building app layout nav

---
+++ resources/views/components/layout/nav.blade.php
@@ -0,0 +1,14 @@
+<nav class="border-b border-border px-6">
+    <div class="max-w-7xl mx=-auto h-16 flex items-center justify-between">
+        <div>
+            <a href="/">
+                <img src="/images/logo.png" alt="Lego Logo" width="100px" />
+            </a>
+        </div>
+
+        <div class="flex gap-x-5 items-center">
+            <a href="">Register</a>
+            <a href="" class="btn">Log In</a>
+        </div>
+    </div>
+</nav>
