d067762a2d9c0d32d85f598e9b8a178552e2aac6
blytheb <blytheballesteros@gmail.com>
2026-04-13 10:24:14 -1000

    relationships

--- app/Models/Idea.php
+++ app/Models/Idea.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use App\IdeaStatus;
+use App\Models\Step;
 use Illuminate\Database\Eloquent\Casts\AsArrayObject;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Idea extends Model
 {
@@ -15,5 +18,16 @@ class Idea extends Model
     protected $casts = [
         'links' => AsArrayObject::class,
         'status' => IdeaStatus::class,
-    ]
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+
+    public function steps(): HasMany
+    {
+        return $this->HasMany(Step::class);
+    }
 }
