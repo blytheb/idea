2b8894f670f988e5fa6741e0faaf9bed09d89f15
blytheb <blytheballesteros@gmail.com>
2026-04-14 09:55:47 -1000

    update routes and redirect, laod user idea, create index view, card/status component

--- app/Http/Controllers/IdeaController.php
+++ app/Http/Controllers/IdeaController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Idea;
 use App\Http\Requests\StoreIdeaRequest;
 use App\Http\Requests\UpdateIdeaRequest;
+use App\Models\Idea;
+use Illuminate\Support\Facades\Auth;
 
 class IdeaController extends Controller
 {
@@ -13,7 +14,11 @@ class IdeaController extends Controller
      */
     public function index()
     {
-        //
+        $ideas = Auth::user()->ideas()->get();
+
+        return view('idea.index', [
+            'ideas' => $ideas
+        ]);
     }
 
     /**
