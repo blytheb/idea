4110cffd62b8639dbb25d9104f9e4c42e2c88502
blytheb <blytheballesteros@gmail.com>
2026-04-17 13:18:16 -1000

    created email notification and profile tests

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Notifications\EmailChanged;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Notification;
 use Illuminate\Validation\Rule;
 use Illuminate\Validation\Rules\Password;
 
@@ -29,11 +31,19 @@ class ProfileController extends Controller
             'password' => ['nullable', Password::defaults()],
         ]);
 
+        $originalEmail = $user->email;
+
         $user->update([
             'name' => $request->name,
             'email' => $request->email,
             'password' => $request->password ?? $user->password,
         ]);
+
+        // if email changed, send email change notification
+        if ($originalEmail !== $request->email) {
+            Notification::route('mail', $originalEmail)
+                ->notify(new EmailChanged($user, $originalEmail));
+        }
 
         return redirect()->route('profile.edit')->with('success', 'Profile updated!');
     }
---
+++ tests/Browser/ProfileTest.php
@@ -0,0 +1,51 @@
+<?php
+
+use App\Models\User;
+use App\Notifications\EmailChanged;
+use Illuminate\Support\Facades\Notification;
+
+it('requires authentication', function () {
+    $this->get(route('profile.edit'))->assertRedirect('/login');
+});
+
+it('edits a profile', function () {
+    $user = User::factory()->create();
+
+    $this->actingAs($user);
+
+    visit(route('profile.edit'))
+        ->assertValue('name', $user->name)
+        ->fill('name', 'New Name')
+        ->assertValue('email', $user->email)
+        ->fill('email', 'new@email.com')
+        ->click('Update Account')
+        ->assertSee('Profile updated');
+
+    expect($user->fresh())->toMatchArray([
+        'name' => 'New Name',
+        'email' => 'new@email.com',
+    ]);
+
+});
+
+it('notifies the original email if updated', function () {
+    $user = User::factory()->create();
+
+    $this->actingAs($user);
+
+    Notification::fake();
+
+    $origionalEmail = $user->email;
+
+    visit(route('profile.edit'))
+
+        ->assertValue('email', $user->email)
+        ->fill('email', 'new@email.com')
+        ->click('Update Account')
+        ->assertSee('Profile updated');
+
+    Notification::assertSentOnDemand(EmailChanged::class, function (EmailChanged $notification, $routes, $notifiable) use ($origionalEmail) {
+        return $notifiable->routes['mail'] === $origionalEmail;
+    });
+
+});
